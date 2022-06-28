a115aad9a78afc9b2e5764591b5f413004f8f662
Walaa Alshaikh <walaalshaikh94@gmail.com>
2022-06-28 08:57:46 +0300

    adding carousel and the change of text size

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    function increaseFontSizeBy100px() {
+        document.getElementById('a').style.fontSize = "25px";
+    }
+    function increaseFontSizeBy1px() {
+       
+        document.getElementById('a').style.fontSize = "16px";
+       
+    }
